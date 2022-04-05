890f965866759c41b8cb805879ad657c124d86e6
MyoMyintAung <myomyintaung064@gmail.com>
2022-04-05 21:05:44 +0630

    fetch data with reduce state management

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,21 +1,42 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import "./Pokemon.css";
+import { Card, Button } from "react-bootstrap";
 
 const pokeConfig = {
   limit: 100,
   offset: 0,
 };
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "MAKE_REQUEST":
+      return { ...state, isLoading: true, pokemon: [] };
+
+    case "GET_DATA":
+      return { ...state, isLoading: false, pokemon: action.payload };
+    case "ERROR":
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+        pokemon: [],
+      };
+    default:
+      return state;
+  }
+};
+
 const Pokemon = () => {
-  const [error, setError] = useState(null);
-  const [pokemon, setPokemon] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [state, dispatch] = useReducer(reducer, {
+    isLoading: true,
+    pokemon: [],
+  });
 
   // fetch name and img then create an array of objects
-  const fetchPokemonHandler = useCallback(async () => {
+  const fetchPokemonHandler = async () => {
     try {
-      setIsLoading(true);
-      setError(null);
+      dispatch({ type: "MAKE_REQUEST" });
+      console.log(state);
 
       const response = await fetch(
         `https://pokeapi.co/api/v2/pokemon?limit=${pokeConfig.limit}&offset=${pokeConfig.offset}`
@@ -38,32 +59,34 @@ const Pokemon = () => {
           };
         });
 
-      setPokemon(pokemonArray);
-    } catch (error) {
-      setError(error.message);
-    }
+      dispatch({ type: "GET_DATA", payload: pokemonArray });
 
-    setIsLoading(false);
-  }, []);
+      console.log(state);
+    } catch (error) {
+      dispatch({ type: "ERROR", payload: error.message });
+    }
+  };
 
   // fetch on mount
   useEffect(() => {
     fetchPokemonHandler();
-  }, [fetchPokemonHandler]);
+  }, []);
 
   let content = <p>No pokemon was found....</p>;
 
   // conditional rendering ui
-  if (isLoading) {
+  if (state.isLoading) {
     content = <p>Loading...</p>;
-  } else if (error) {
-    content = <p>{error}</p>;
-  } else if (pokemon.length > 0) {
-    content = pokemon.map((poke) => (
-      <li className="card" key={poke.name}>
-        <img src={poke.img} alt={poke.name} />
-        <p>{poke.name}</p>
-      </li>
+  } else if (state.error) {
+    content = <p>{state.error}</p>;
+  } else if (state.pokemon.length > 0) {
+    content = state.pokemon.map((poke) => (
+      <Card key={poke.name} style={{ width: "8rem" }}>
+        <Card.Img variant="top" src={poke.img} />
+        <Card.Body>
+          <Card.Title>{poke.name}</Card.Title>
+        </Card.Body>
+      </Card>
     ));
   }
 
